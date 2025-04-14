b4981beac8efd8de9e7130852f6b42813e116b27
IOexistDa <49242746+IOexistDa@users.noreply.github.com>
2025-04-14 12:06:40 +0300

    Add files via upload

---
+++ SimpleSender.ino
@@ -0,0 +1,63 @@
+/*
+ * SimpleSender.cpp
+ *
+ *  Demonstrates sending IR codes in standard format with address and command
+ *  An extended example for sending can be found as SendDemo.
+ *
+ *  Copyright (C) 2020-2022  Armin Joachimsmeyer
+ *  armin.joachimsmeyer@gmail.com
+ *
+ *  This file is part of Arduino-IRremote https://github.com/Arduino-IRremote/Arduino-IRremote.
+ *
+ *  MIT License
+ */
+#include <Arduino.h>
+
+#define DISABLE_CODE_FOR_RECEIVER // Disables restarting receiver after each send. Saves 450 bytes program memory and 269 bytes RAM if receiving functions are not used.
+//#define SEND_PWM_BY_TIMER         // Disable carrier PWM generation in software and use (restricted) hardware PWM.
+//#define USE_NO_SEND_PWM           // Use no carrier PWM, just simulate an active low receiver signal. Overrides SEND_PWM_BY_TIMER definition
+
+/*
+ * This include defines the actual pin number for pins like IR_RECEIVE_PIN, IR_SEND_PIN for many different boards and architectures
+ */
+#include "PinDefinitionsAndMore.h"
+#include <IRremote.hpp> // include the library
+
+const int buttonPin = 2;
+
+int buttonState = 0;
+
+void setup() {
+    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(buttonPin, INPUT);
+
+    Serial.begin(115200);
+
+    // Just to know which program is running on my Arduino
+    Serial.println(F("START " __FILE__ " from " __DATE__ "\r\nUsing library version " VERSION_IRREMOTE));
+    Serial.print(F("Send IR signals at pin "));
+    Serial.println(IR_SEND_PIN);
+
+    IrSender.begin(DISABLE_LED_FEEDBACK); // Start with IR_SEND_PIN as send pin and disable feedback LED at default feedback LED pin
+}
+
+
+uint8_t sCommand = 0x34;
+uint8_t sRepeats = 0;
+
+void loop() {
+    
+    buttonState = digitalRead(buttonPin);
+
+    if (buttonState == HIGH) {
+    // turn LED on:
+    Serial.println(F("Send standard NEC with 8 bit address"));
+    Serial.flush();
+
+    // Receiver output for the first loop must be: Protocol=NEC Address=0x102 Command=0x34 Raw-Data=0xCB340102 (32 bits)
+    IrSender.sendNEC(0x102, sCommand, sRepeats);
+
+    delay(1000);
+  } 
+
+}
